e51ff348e646b61d0742d24907f8157dce964c5e
azamuzapbar <azamuzapbar@gmail.com>
2024-12-14 14:25:31 +0500

    Второй этап, добавление выбора формата

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
@@ -7,7 +8,14 @@ def main():
     )
     parser.add_argument('first_file', help='Path to the first file')
     parser.add_argument('second_file', help='Path to the second file')
+    parser.add_argument('-f', '--format',
+                        choices=['plain', 'json'],
+                        default='plain',
+                        help='set format of output (default: plain)')
     args = parser.parse_args()
+    print(f'Comparing {args.first_file} with {args.second_file}')
+    print(f'Output format: {args.format}')
+
 
 if __name__ == '__main__':
     main()
