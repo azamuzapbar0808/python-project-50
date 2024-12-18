0a1127f477fc6aaf203e2844eed700d554f328ac
azamuzapbar <azamuzapbar@gmail.com>
2024-12-18 10:44:58 +0500

    Подключение тестов

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/azamuzapbar0808/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/azamuzapbar0808/python-project-50/actions)
+
+![CI](https://github.com/<your-username>/<repo-name>/actions/workflows/main.yml/badge.svg)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/<badge-id>/test_coverage)](https://codeclimate.com/github/<your-username>/<repo-name>/test_coverage)
--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,4 +1,25 @@
 import argparse
+import json
+
+def generate_diff(file_path1, file_path2):
+    with open(file_path1, 'r') as file1, open(file_path2, 'r') as file2:
+        data1 = json.load(file1)
+        data2 = json.load(file2)
+
+    all_keys = sorted(set(data1.keys()).union(data2.keys()))
+    diff = []
+
+    for key in all_keys:
+        if key in data1 and key in data2:
+            if data1[key] != data2[key]:
+                diff.append(f'  - {key}: {data1[key]}')
+                diff.append(f'  + {key}: {data2[key]}')
+        elif key in data1:
+            diff.append(f'  - {key}: {data1[key]}')
+        elif key in data2:
+            diff.append(f'  + {key}: {data2[key]}')
+
+    return '\n'.join(diff)
 
 
 def main():
@@ -13,8 +34,10 @@ def main():
                         default='plain',
                         help='set format of output (default: plain)')
     args = parser.parse_args()
+
     print(f'Comparing {args.first_file} with {args.second_file}')
-    print(f'Output format: {args.format}')
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':
